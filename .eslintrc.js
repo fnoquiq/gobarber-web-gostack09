aa6367a348c2b8a83b84a57de6a883170636e33a
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-12-21 02:06:12 -0300

    :rocket: Configura Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,4 +33,11 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
+  setting: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
