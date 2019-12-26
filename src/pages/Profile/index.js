556c00e3e57dc01339d7ba4b923479e99f349de7
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-12-25 23:26:11 -0300

    :rocket: Adiciona inserção de fotos de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,8 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
+import AvatarInput from './AvatarInput';
+
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
@@ -17,6 +19,8 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de email" />
 
